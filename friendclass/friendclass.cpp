cd2c01f7b4dbb86bf2de3d33253ce5fa07d083e7
gheeeeeeee <gheoputra55@gmail.com>
2024-05-06 15:22:42 +0700

    friend class 1

--- friendclass/friendclass.cpp
+++ friendclass/friendclass.cpp
@@ -5,26 +5,46 @@ using namespace std;
 class siswa;
 class orang
 {
-private :
+private:
     string nama;
 
-public :
-    void setnama(string pNama);
+public:
+    void setNama(string pNama);
     friend class siswa;
+};
 
     class siswa
     {
-    private :
+    private:
         int id;
     public:
         void setId(int pId);
-        void displayALL(orang& a);
+        void displayA11(orang& a);
+    };
 
-        
+    void siswa::displayA11(orang &a)
+    {
+        cout << id << endl<< a.nama;
+    }
+
+    void orang::setNama(string pNama)
+    {
+        nama = pNama;
+    }
+    void siswa::setId(int pId)
+    {
+        id = pId;
+    }
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayA11(joko);
+    return 0;
 }
 
 

63ec66f63ba3133444041b5bddc50c173621c8f1
gheeeeeeee <gheoputra55@gmail.com>
2024-05-06 15:39:03 +0700

    friend function kedua

--- friendfunction2/friendfunction2.cpp
+++ friendfunction2/friendfunction2.cpp
@@ -11,9 +11,28 @@ public:
     void showNilaiPelajar(pelajar& x);
 };
 
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
+void manusia::showNilaiPelajar(pelajar& x)
+{
+    cout << x.nilai;
+}
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
 }
 
 

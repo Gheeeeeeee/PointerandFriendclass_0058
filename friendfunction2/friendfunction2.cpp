37db7dcd9c58c8c3c96337c88c8360c339eda336
gheeeeeeee <gheoputra55@gmail.com>
2024-05-06 15:30:59 +0700

    class = manusia
    modifier public

---
+++ friendfunction2/friendfunction2.cpp
@@ -0,0 +1,19 @@
+// friendfunction2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class pelajar;
+class manusia
+{
+public:
+    void showNilaiPelajar(pelajar& x);
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
